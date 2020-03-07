3ec821ec2abb079282838d9fdc16ef7e24c63293
choidevs <61908874+choidevs@users.noreply.github.com>
2020-03-07 23:21:43 +0900

    Add files via upload

---
+++ common/service.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+#setenforce 0
+#resetprop ro.product.model Pixel3a
+#resetprop ro.build.id `getprop ro.build.id`'
